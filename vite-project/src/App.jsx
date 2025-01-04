0581b9c0e3424ac346fd45a0ffa4f1cab146aa87
Mikaeel92 <mikaeelyaghoobzadeh@gmail.com>
2025-01-04 12:10:46 +0330

    state & useEffect

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const App = () => {
+
+const [darkMode, setDarkMode] = useState(false)
+
+useEffect(() => {
+  let saveMode = localStorage.getItem('displayMode')
+  if(!saveMode) {
+    saveMode = 'light'
+    setDarkMode(false)
+    localStorage.setItem('displayMode', saveMode)
+  }
+  setDarkMode(saveMode === 'dark' ? true : false)
+}, [])
+
   return (
     <div>App</div>
   )
